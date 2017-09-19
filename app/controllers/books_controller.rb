ea5e184032ce9379b7a41f077c0e39ee60dc341b
Colin Di Meo <dimeo.colin@gmail.com>
2017-09-19 14:04:53 -0400

    added error handling on invalid for submission

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,28 @@
 class BooksController < ApplicationController
  def index
-   @books = Book.all
+  @books = Book.all
  end
 
  def show
-   @book = Book.find(params[:id])
+  @book = Book.find(params[:id])
  end
 
  def new
+   @book = Book.new
  end
 
  def create
   @book = Book.new(book_params)
 
-  @book.save
-  redirect_to @book
+  if @book.save
+   redirect_to @book
+  else
+   render 'new'
+  end
  end
 
  private
+
  def book_params
   params.require(:book) .permit(:title, :author, :description, :link)
  end
